664ccdb82c66169672148d130b8f86ad93ee8dcc
laravaletto <lvaletto21@gmail.com>
2025-04-27 02:43:09 -0300

    avances navbar

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,15 +1,20 @@
 import './App.css'
 import { Route, Routes } from 'react-router-dom'
+/* Common*/
+import HeaderNavbar from "./components/common/HeaderNavbar";
+/* Pages */
 import { HomePage } from './pages/HomePage'
 
 function App() {
 
   return (
     <>
+        <HeaderNavbar />
       <Routes>
         <Route path='/' element={<HomePage/>} />
       </Routes>
     </>
+
   )
 }
 
--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -1,6 +1,22 @@
+/*export const HomePage = () => {
+  return (
+    <div className="text-xl text-red-500">HomePage</div>
+  )
+  
+}*/
 
 export const HomePage = () => {
   return (
-    <div className="text-xl text-red-500">HomePage</div>
+    <div>
+        <div className="text-center mt-5">
+        <div className="text-xl text-red-500">
+          HomePage
+        </div>
+
+        {/* Aquí agregamos el botón de Bootstrap */}
+        <button className="btn btn-primary btn-lg mt-4">¡Haz clic aquí!</button>
+        </div>
+    </div>
+    
   )
 }
