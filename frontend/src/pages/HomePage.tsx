25e37f9d623326b021feab0619a598d653e3eaac
laravaletto <lvaletto21@gmail.com>
2025-04-28 13:39:15 -0300

    avances HomePage 2da parte

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import '../index.css'; // Importamos el archivo CSS
+
 
 import { Link } from 'react-router-dom';
 
 const HomePage = () => {
 
   return (
+    <div>
     <div
       className="position-relative"
       style={{
@@ -14,8 +17,7 @@ const HomePage = () => {
         backgroundPosition: "center",
         height: "80vh",
         color: "white",
-      }}
-    >
+      }}>
       <div
         className="position-absolute top-50 start-0 translate-middle-y text-start"
         style={{ textShadow: "0 2px 4px rgba(165, 192, 161, 0.7)", paddingLeft: "2rem",}}>
@@ -31,6 +33,52 @@ const HomePage = () => {
             </Link>
           </button>
       </div>
+      </div>
+
+      {/* Sección de servicios de salud */}
+      <section className="servicios-section bg-white">
+      <div className="container py-5 text-center">
+        {/* Encabezado */}
+        <div className="row mb-5">
+          <div className="col">
+            <h1 className="servicios-title">Cuidando de tu salud</h1>
+            <p className="servicios-descripcion">
+              Conoce nuestros servicios
+            </p>
+          </div>
+        </div>
+
+        {/* Servicios */}
+        <div className="row g-3">
+          {/* Card 1 */}
+          <div className="col-lg-3 col-md-6">
+            <div className="card h-100">
+              <div className="card-body text-center">
+                <img
+                  src=""
+                  alt=""
+                  className="staff-img"
+                />
+                <h4 className="staff-name">Titulo</h4>
+                <h5 className="staff-cargo">Parrafo</h5>
+                <div className="d-flex flex-row justify-content-center">
+                  <div className="p-4">
+                    <a>
+                     Texto
+                    </a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+
+
+          {/* Puedes continuar agregando más cards */}
+        </div>
+      </div>
+    </section>
+
+
     </div>
   );
 };
