e0790a577bbda6402e942b834f410ec422377ff6
Vargas Jonatan <jonatan.vargasportillo@gmail.com>
2025-04-29 00:27:46 -0300

    se agrego el fakeAuth para poder usar los endpoints

--- backend/src/routes/api.routes.ts
+++ backend/src/routes/api.routes.ts
@@ -1,8 +1,11 @@
 import { Router, Request, Response } from "express";
 import usersRouter from "./usersRoutes";
 import turnoRouter from "./TurnoRoutes";
+import { fakeAuth } from "../middlewares/fakeAuth";
 
 const router = Router();
+
+router.use(fakeAuth);
 
 // Ejemplo de endpoints
 router.use("/users",usersRouter);
