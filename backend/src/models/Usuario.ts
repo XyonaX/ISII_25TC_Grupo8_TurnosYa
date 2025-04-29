b9425230b8094f77280edec86b074876e9945665
Vargas Jonatan <jonatan.vargasportillo@gmail.com>
2025-04-29 17:52:57 -0300

    agregue el tipo_usuario par poder diferenciar en el handler a la hora de mandarlo por el payload

--- backend/src/models/Usuario.ts
+++ backend/src/models/Usuario.ts
@@ -1,78 +1,81 @@
 import { Schema, model } from "mongoose";
 
-const UsuarioSchema = new Schema({
-    dni_usuario: {
-        type: String,
-        required: true,
-        unique: true,
-        trim: true
+const UsuarioSchema = new Schema(
+    {
+        dni_usuario: {
+            type: String,
+            required: true,
+            unique: true,
+            trim: true,
+        },
+        nombre_usuario: {
+            type: String,
+            required: true,
+            trim: true,
+        },
+        apellido_usuario: {
+            type: String,
+            required: true,
+            trim: true,
+        },
+        fecha_nac_usuario: {
+            type: Date,
+            required: true,
+        },
+        celular_usuario: {
+            type: String,
+            required: true,
+            trim: true,
+        },
+        email_usuario: {
+            type: String,
+            required: true,
+            unique: true,
+            lowercase: true,
+            trim: true,
+        },
+        tipo_usuario: {
+            //De momento vamos a dejarle el tipo de usuario... 
+            type: String,
+            enum: ["paciente", "medico"],
+            required: true,
+        },
+        clave_usuario: {
+            type: String,
+            required: true,
+        },
+        calle_usuario: {
+            type: String,
+            required: true,
+            trim: true,
+        },
+        num_usuario: {
+            type: String,
+            required: true,
+            trim: true,
+        },
+        dpto_usuario: {
+            type: String,
+            trim: true,
+        },
+        cod_postal: {
+            type: String,
+            required: true,
+            trim: true,
+        },
+        id_ciudad: {
+            type: Schema.Types.ObjectId,
+            ref: "Ciudad",
+            required: true,
+        },
+        id_estado_usuario: {
+            type: Schema.Types.ObjectId,
+            ref: "EstadoUsuario",
+            required: true,
+        },
     },
-    nombre_usuario: {
-        type: String,
-        required: true,
-        trim: true
-    },
-    apellido_usuario: {
-        type: String,
-        required: true,
-        trim: true
-    },
-    fecha_nac_usuario: {
-        type: Date,
-        required: true
-    },
-    celular_usuario: {
-        type: String,
-        required: true,
-        trim: true
-    },
-    email_usuario: {
-        type: String,
-        required: true,
-        unique: true,
-        lowercase: true,
-        trim: true
-    },
-    clave_usuario: {
-        type: String,
-        required: true
-    },
-    calle_usuario: {
-        type: String,
-        required: true,
-        trim: true
-    },
-    num_usuario: {
-        type: String,
-        required: true,
-        trim: true
-    },
-    dpto_usuario: {
-        type: String,
-        trim: true
-    },
-    cod_postal: {
-        type: String,
-        required: true,
-        trim: true
-    },
-    tipo_usuario: {
-        type: String,
-        required: true,
-        enum: ["paciente", "medico"]
-    },
-    id_ciudad: {
-        type: Schema.Types.ObjectId,
-        ref: "Ciudad",
-        required: true
-    },
-    id_estado_usuario: {
-        type: Schema.Types.ObjectId,
-        ref: "EstadoUsuario",
-        required: true
-    }
-}, { timestamps: true });
-
+    { timestamps: true }
+);
 
 const Usuario = model("Usuario", UsuarioSchema);
 
